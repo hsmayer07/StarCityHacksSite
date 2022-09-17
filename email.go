36967673c2b8af4a94cdc40bfa9056d54f74d86d
Henry Mayer-School <henrymayer-school@pal-nat186-182-128.itap.purdue.edu>
2022-09-16 23:32:50 -0400

    Using Go

--- email.go
+++ email.go
@@ -1,10 +1,46 @@
-package handler
+package main
 
 import (
-  "fmt"
-  "net/http"
+    "fmt"
+    "log"
+    "html/template"
+    "net/http"
 )
 
-func Handler(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
+type ContactDetails struct {
+    Name    string
+    Email   string
+    Subject string
+    Message string
+}
+
+func main() {
+    tmpl, err := template.ParseFiles("contact.html")
+    if err != nil {
+        log.Fatal("Parse: ", err)
+        return
+    }
+
+    http.HandleFunc("/contact", func(w http.ResponseWriter, r *http.Request) {
+        if r.Method != http.MethodPost {
+            tmpl.Execute(w, nil)
+            return
+        }
+
+        // get form data
+        formData := ContactDetails{
+            Name:   r.FormValue("name"),
+            Email:   r.FormValue("email"),
+            Subject: r.FormValue("subject"),
+            Message: r.FormValue("message"),
+        }
+
+        // do something with the submitted form data
+        fmt.Printf("%+v\n", formData)
+
+        // response
+        tmpl.Execute(w, struct{ Success bool }{true})
+    })
+
+    http.ListenAndServe(":80", nil)
 }
